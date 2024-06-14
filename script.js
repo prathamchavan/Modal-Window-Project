268c4680b6932b1e32f233acec017fe38c8736ce
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-06-15 00:13:15 +0530

    using classes and eventlistners

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  console.log(btnOpenModal[i].addEventListener("click", openModal));
+}
+
+//here we have given written 'closeModal' and not 'closeModal()'
+//because as soon as we write the 'closeModal()' javaScript will
+// execute the function even if user do not click on the text
+// so we write 'closeModal' so that event will execute on only on
+// when user will click on the code
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
