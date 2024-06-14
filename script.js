fdc0dfa7e5c846f444f8c160dd958e17b82aa85b
Prathamesh Chavan <prathamchavan0013@gmail.com>
2024-06-15 00:12:07 +0530

    Working with the classes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
